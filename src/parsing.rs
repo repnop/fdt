799740d7a60fadb593a83140545b7356cc3f82fa
Property404 <git@dagans.dev>
2023-12-16 12:01:46 -0600

    Switch to Rust edition 2021

--- src/parsing.rs
+++ src/parsing.rs
@@ -2,7 +2,6 @@
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at https://mozilla.org/MPL/2.0/.
 
-use core::convert::TryInto;
 pub struct CStr<'a>(&'a [u8]);
 
 impl<'a> CStr<'a> {
